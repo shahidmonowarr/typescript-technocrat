19136135e8f6812f119ba603a10521501b15af42
shahidmonowarr <shahidmonowar21@gmail.com>
2023-05-06 22:55:53 +0600

    polymorphism

---
+++ src/module-4/polymorphism.ts
@@ -0,0 +1,77 @@
+class Person {
+    takeNap() {
+        console.log('Sleeping')
+    }
+}
+
+class Student1 extends Person {
+    takeNap() {
+        console.log('Studying')
+    }
+}
+
+class Employee extends Person {
+    takeNap() {
+        console.log('Working')
+    }
+}
+
+class Teacher1 extends Person {
+    takeNap() {
+        console.log('Teaching')
+    }
+}
+
+function getNap(person: Person) {
+    person.takeNap();
+}
+
+const student4 = new Student1();
+const employee = new Employee();
+const teacher4 = new Teacher1();
+getNap(student4);
+getNap(employee);
+getNap(teacher4);
+
+
+// another example
+class Shape{
+    getArea(): number{
+        return 0;
+    }
+}
+
+// area => pi * r * r
+class Circle extends Shape{
+    radius: number;
+    constructor(radius: number){
+        super();
+        this.radius = radius;
+    }
+    getArea(): number{
+    return Math.PI * this.radius * this.radius;
+}
+}
+
+class Rectangle extends Shape{
+    length: number;
+    breadth: number;
+    constructor(length: number, breadth: number){
+        super();
+        this.length = length;
+        this.breadth = breadth;
+    }
+    getArea(): number{
+    return this.length * this.breadth;
+}
+}
+
+function getAreaOfShape(shape: Shape){
+    console.log(shape.getArea());
+}
+
+getAreaOfShape(new Circle(10));
+getAreaOfShape(new Rectangle(10, 20));
+
+
+
